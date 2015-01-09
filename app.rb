b099e840cf25150bf55dbc97de2da47638fa7bd8
Mike Goren <mgoren@mortalwombat.net>
2015-01-09 11:59:30 -0800

    add more prettification

--- app.rb
+++ app.rb
@@ -12,17 +12,18 @@ get('/wordcount') do
   word = params.fetch('word')
 
   if phrase == "" || word == ""
-    @message = "Invalid input. Please fill in both input fields."
+    @message = "<span class='error'>Invalid input. Please fill in both input fields.</span>"
   elsif word.include?(" ")
-    @message = "Invalid input. Please search for a single word only."
+    @message = "<span class='error'>Invalid input. Please search for a single word only.</span>"
   else
     counter = phrase.wordcount(word)
-    if counter == 1
-      time_word = " time"
+    if counter == 0
+      @message = "I could not locate the word &ldquo;<span class='word'>".concat(word).concat("</span>&rdquo; in your text.")
+    elsif counter == 1
+      @message = "I found the word &ldquo;<span class='word'>".concat(word).concat("</span>&rdquo; ").concat(counter.to_s).concat(" time in your text.")
     else
-      time_word = " times"
+      @message = "I found the word &ldquo;<span class='word'>".concat(word).concat("</span>&rdquo; ").concat(counter.to_s).concat(" times in your text.")
     end
-    @message = "I found the word &ldquo;".concat(word).concat("&rdquo; ").concat(counter.to_s).concat(time_word).concat(" in your text.")
   end 
 
   erb(:wordcount)
