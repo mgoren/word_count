bcf1ab1ea337de721731c4c390540bfd8e13165a
Mike Goren <mgoren@mortalwombat.net>
2015-01-09 12:12:08 -0800

    add additional beautification

--- app.rb
+++ app.rb
@@ -12,17 +12,17 @@ get('/wordcount') do
   word = params.fetch('word')
 
   if phrase == "" || word == ""
-    @message = "<span class='error'>Invalid input. Please fill in both input fields.</span>"
+    @message = "<span id='error'>Invalid input. Please fill in both input fields.</span>"
   elsif word.include?(" ")
-    @message = "<span class='error'>Invalid input. Please search for a single word only.</span>"
+    @message = "<span id='error'>Invalid input. Please search for a single word only.</span>"
   else
     counter = phrase.wordcount(word)
     if counter == 0
-      @message = "I could not locate the word &ldquo;<span class='word'>".concat(word).concat("</span>&rdquo; in your text.")
+      @message = "I was unable to locate the word &ldquo;<span id='word'>".concat(word).concat("</span>&rdquo; in your text.")
     elsif counter == 1
-      @message = "I found the word &ldquo;<span class='word'>".concat(word).concat("</span>&rdquo; ").concat(counter.to_s).concat(" time in your text.")
+      @message = "I found the word &ldquo;<span id='word'>".concat(word).concat("</span>&rdquo; ").concat(counter.to_s).concat(" time in your text.")
     else
-      @message = "I found the word &ldquo;<span class='word'>".concat(word).concat("</span>&rdquo; ").concat(counter.to_s).concat(" times in your text.")
+      @message = "I found the word &ldquo;<span id='word'>".concat(word).concat("</span>&rdquo; ").concat(counter.to_s).concat(" times in your text.")
     end
   end 
 
